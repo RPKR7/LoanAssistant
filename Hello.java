0b998690af807b7dfc33a28fddf6e162f51a5eec
RPKR7 <82303151+RPKR7@users.noreply.github.com>
2022-07-13 16:33:04 +0530

    2nd update

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello{
   public static void main(String[] a){
-    System.out.println("Hello MF");
+    System.out.println("Hello");
+    
+    System.out.println("Don't say hello say hai");
   }
 }
